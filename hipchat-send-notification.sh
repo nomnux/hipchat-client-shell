55f4bb98ebe838eff7c24109f822adf272549cc7
nomura <nomura@example.jp>
2016-08-03 19:42:18 +0900

    Add from parameter.

--- hipchat-send-notification.sh
+++ hipchat-send-notification.sh
@@ -3,12 +3,14 @@
 set -e
 
 if [ $# -eq 0 ]; then
-    echo "Usage: ${0##*/} MESSAGE"
+    echo "Usage: ${0##*/} FROM MESSAGE"
     exit 1
 fi
 
 . "$HOME"/.HipChat/credentials
 
+from="$1"
+shift
 msg="$1"
 
 base_uri="https://api.hipchat.com"
@@ -19,6 +21,7 @@ curl \
   -H "Authorization: Bearer $token" \
   -H "Content-Type: application/x-www-form-urlencoded" \
   -X POST \
+  --data-urlencode "from=$from" \
   --data-urlencode "color=yellow" \
   --data-urlencode "message_format=text" \
   --data-urlencode "notify=true" \
